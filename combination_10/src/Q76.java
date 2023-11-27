92b899e1a86da661948c1c222cad860a27f06e7d
Caution-Sun <hisbrjseka414>
2023-11-27 17:15:32 +0900

    Q76

---
+++ combination_10/src/Q76.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q76 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[][] D = new int[N + 1][N + 1];
+
+        for(int i = 0; i < N + 1; i++){
+            D[i][1] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
+        }
+
+        for(int i = 2; i < N + 1; i++){
+            for(int j = 1; j < i; j++){
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        System.out.println(D[N][K]);
+    }
+}
